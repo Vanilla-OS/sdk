e21e320a56b1910f8bb3d70e6b2232301d896bc0
mirkobrombin <send@mirko.pm>
2024-02-05 22:51:50 +0100

    feat: add support to pivot

---
+++ pkg/v1/chroot/pivot.go
@@ -0,0 +1,87 @@
+package chroot
+
+import (
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+// EnterPivot enters a pivot and changes the current working directory
+// to the new root. It returns a function that exits the pivot and
+// changes the current working directory to the old root. Remember to
+// call this function after you are done with the pivot to avoid
+// leaving the process in a pivot.
+//
+// Example:
+//
+//	fExit, err := chroot.EnterPivot("/path/to/new/root")
+//	if err != nil {
+//		fmt.Printf("Error entering pivot root: %v\n", err)
+//		if fExit != nil {
+//			fExit()
+//		}
+//		return
+//	}
+//	defer fExit()
+func EnterPivot(rootFs string) (f func() error, err error) {
+	fd, err := os.Open("/")
+	if err != nil {
+		return nil, err
+	}
+
+	closeFunc := func() error {
+		defer fd.Close()
+		if err := fd.Chdir(); err != nil {
+			return err
+		}
+		return syscall.PivotRoot(".", rootFs)
+	}
+
+	pivotDir := filepath.Join(rootFs, ".pivot_root")
+	err = os.MkdirAll(pivotDir, 0755)
+	if err != nil {
+		return closeFunc, err
+	}
+
+	err = syscall.PivotRoot(rootFs, pivotDir)
+	if err != nil {
+		return closeFunc, err
+	}
+
+	err = os.Chdir("/")
+	if err != nil {
+		return closeFunc, err
+	}
+
+	return closeFunc, nil
+}
+
+// RunPivot runs a function in a pivot and changes the current working
+// environment to the new root. It exits the pivot after the function
+// returns. The function returns the error returned by the function and
+// the error returned by ExitPivot if any.
+//
+// Example:
+//
+//	err := chroot.RunPivot("/path/to/new/root", func() error {
+//		return exec.Command("ls", "-l").Run()
+//	})
+func RunPivot(rootFs string, f func() error) error {
+	fExit, err := EnterPivot(rootFs)
+	if err != nil {
+		return err
+	}
+	defer fExit()
+
+	return f()
+}
+
+// ExitPivot exits the pivot and changes the current working directory
+// to the old root. It returns an error if any.
+//
+// Example:
+//
+//	err := chroot.ExitPivot()
+func ExitPivot() error {
+	return syscall.PivotRoot(".", ".")
+}
--- pkg/v1/chroot/tests/chroot_test.go
+++ pkg/v1/chroot/tests/chroot_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/vanilla-os/sdk/pkg/v1/chroot"
+	"github.com/vanilla-os/sdk/pkg/v1/system"
 )
 
 // This function tests both EnterChroot, ExitChroot and RunChroot, so
@@ -59,6 +60,62 @@ func TestRunChroot(t *testing.T) {
 
 	// Check if we are still in the old root by reading /etc/os-release
 	osReleaseFile := filepath.Join("/etc", "os-release")
+	osRelease, err := os.ReadFile(osReleaseFile)
+	if err != nil {
+		t.Fatalf("Failed to read /etc/os-release: %v", err)
+	}
+	t.Logf("Distribution (from outside): %s", osRelease)
+}
+
+// Same as TestRunChroot but for pivot operations
+func TestRunPivot(t *testing.T) {
+	// Skip the test if not running as root
+	if os.Geteuid() != 0 {
+		t.Skip("Skipping test: root privileges required for pivot operations")
+	}
+
+	// And also skip if we are inside a container
+	if system.RunningInContainer() {
+		t.Skip("Skipping test: pivot operations are not supported inside a container")
+	}
+
+	// Download Alpine tarball to use as the new root filesystem
+	alpineURL := "https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-minirootfs-3.19.0-x86_64.tar.gz"
+	tmpDir := t.TempDir()
+	alpineTarball := filepath.Join(tmpDir, "alpine-minirootfs.tar.gz")
+
+	err := downloadFile(alpineURL, alpineTarball)
+	if err != nil {
+		t.Fatalf("Failed to download Alpine tarball: %v", err)
+	}
+	alpineRoot := filepath.Join(tmpDir, "alpine-root")
+	err = os.Mkdir(alpineRoot, 0755)
+	if err != nil {
+		t.Fatalf("Failed to create Alpine root directory: %v", err)
+	}
+	err = extractTarball(alpineTarball, alpineRoot)
+	if err != nil {
+		t.Fatalf("Failed to extract Alpine tarball: %v", err)
+	}
+
+	// Run a function in the pivot root
+	fErr := chroot.RunPivot(alpineRoot, func() error {
+		// Check if we are in the new root by reading /etc/os-release
+		osReleaseFile := filepath.Join("/etc", "os-release")
+		osRelease, err := os.ReadFile(osReleaseFile)
+		if err != nil {
+			return err
+		}
+		t.Logf("Distribution (from inside): %s", osRelease)
+
+		return nil
+	})
+	if fErr != nil {
+		t.Fatalf("Function in pivot root failed: %v", fErr)
+	}
+
+	// Check if we are still in the old root by reading /etc/os-release
+	osReleaseFile := filepath.Join(alpineRoot, "/etc", "os-release")
 	osRelease, err := os.ReadFile(osReleaseFile)
 	if err != nil {
 		t.Fatalf("Failed to read /etc/os-release: %v", err)
