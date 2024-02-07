6785ed46a431644ebf0df73b59b4e85b3451f919
mirkobrombin <send@mirko.pm>
2024-02-07 20:16:58 +0100

    feat: generate refs index

--- scripts/gen-docs.sh
+++ scripts/gen-docs.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 prefix="github.com/vanilla-os/sdk"
+versions=$(ls pkg)
 
-for pkg_version in $(ls pkg); do
-  for module in $(ls pkg/$pkg_version); do
+# Generate references for each module
+for pkg_version in $versions; do
+  modules=$(ls pkg/$pkg_version)
+
+  mkdir -p docs/references/$pkg_version
+  echo "# $pkg_version\n" > docs/references/$pkg_version/README.md
+
+  for module in $modules; do
     if [ $module = "VERSION" ]; then
       continue
     fi
 
     echo "Generating refs for $prefix/pkg/$pkg_version/$module"
-    mkdir -p docs/references/$pkg_version
     godoc2md $prefix/pkg/$pkg_version/$module > docs/references/$pkg_version/$module.md
-    done
+
+    echo "- [$module](/references/$pkg_version/$module.md)" >> docs/references/$pkg_version/README.md
+  done
 done
